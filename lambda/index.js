589f1075ff62e3a6ff4992152a69f32769174dd0
shane-jung <71043373+shane-jung@users.noreply.github.com>
2023-08-04 14:58:08 -0500

    lambda function update

--- lambda/index.js
+++ lambda/index.js
@@ -1,8 +1,8 @@
-exports.handler = async (event, context) => {
-  console.log("Received event:", JSON.stringify(event, null, 2))
-  console.log("Function name:", context.functionName)
-  return {
+export const handler = async (event) => {
+  // TODO implement
+  const response = {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda!" }),
+    body: JSON.stringify("Hello from Lambda!"),
   }
+  return response
 }
