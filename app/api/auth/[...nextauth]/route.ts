2ef43ee52ef2790c1ee0f4b36da35706f28573d3
shane-jung <71043373+shane-jung@users.noreply.github.com>
2023-09-12 11:31:55 -0500

    compatibility with NextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
-import { prisma } from "@/lib/prisma";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";  
+import { prisma } from "@/lib/prisma"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
-import NextAuth from "next-auth/next";
-import type { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth/next"
+import type { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
 
-export const OPTIONS: NextAuthOptions = {
+const OPTIONS: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -13,7 +13,7 @@ export const OPTIONS: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-};
+}
 
-const handler = NextAuth(OPTIONS);
-export { handler as GET, handler as POST };
+const handler = NextAuth(OPTIONS)
+export { handler as GET, handler as POST }
